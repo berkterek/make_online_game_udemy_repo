de2bf7f9fe029eedb9dcbba8bc577551136a2fe7
berkterek <42406263+berkterek@users.noreply.github.com>
2023-10-26 15:44:12 +0300

    refactor: authentication improvement done

--- MakeOnlineGame/Assets/_GameFolders/Scripts/Concretes/Networks/Clients/AuthenticationWrapper.cs
+++ MakeOnlineGame/Assets/_GameFolders/Scripts/Concretes/Networks/Clients/AuthenticationWrapper.cs
@@ -12,27 +12,65 @@ namespace MakeOnlineGame.Networks.Clients
         {
             if (AuthenticateState == AuthenticateState.Auhenticated) return AuthenticateState;
 
-            int tryValue = 0;
+            await SingInAnonymouslyAsync(maxTry);
+
+            if (AuthenticateState == AuthenticateState.Authenticating ||
+                AuthenticateState == AuthenticateState.NotAuthenticated)
+            {
+                await AuthenticatingAsync();
+                return AuthenticateState;
+            }
+
+            return AuthenticateState;
+        }
+
+        private static async UniTask<AuthenticateState> AuthenticatingAsync()
+        {
+            while (AuthenticateState == AuthenticateState.Authenticating ||
+                   AuthenticateState == AuthenticateState.NotAuthenticated)
+            {
+                await UniTask.Delay(200);
+            }
+
+            return AuthenticateState;
+        }
+
+        private static async UniTask SingInAnonymouslyAsync(int maxTry)
+        {
+            AuthenticateState = AuthenticateState.Authenticating;            
             
-            AuthenticateState = AuthenticateState.Authenticating;
+            int tryValue = 0;
             
             while (AuthenticateState == AuthenticateState.Authenticating && tryValue < maxTry)
             {
-                await AuthenticationService.Instance.SignInAnonymouslyAsync();
-
-                if (AuthenticationService.Instance.IsSignedIn && AuthenticationService.Instance.IsAuthorized)
+                try
                 {
-                    AuthenticateState = AuthenticateState.Auhenticated;
+                    await AuthenticationService.Instance.SignInAnonymouslyAsync();
+
+                    if (AuthenticationService.Instance.IsSignedIn && AuthenticationService.Instance.IsAuthorized)
+                    {
+                        AuthenticateState = AuthenticateState.Auhenticated;
+                    }
+                }
+                catch (System.Exception e)
+                {
+                    Debug.LogError(e);
+                    AuthenticateState = AuthenticateState.Error;
+                    break;
                 }
 
-                Debug.Log(AuthenticateState);
-                
                 tryValue++;
 
                 await UniTask.Delay(1000);
             }
-
-            return AuthenticateState;
+            
+            if (AuthenticateState != AuthenticateState.Auhenticated)
+            {
+                AuthenticateState = AuthenticateState.Timeout;
+                Debug.Log($"Player was not signed in successfully try value: {tryValue}");
+            }
+            
+            Debug.Log(AuthenticateState);
         }
     }
 
