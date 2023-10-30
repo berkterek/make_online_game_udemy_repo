094bf26e48c202c61a3745d279564b5f7f4a634c
berkterek <42406263+berkterek@users.noreply.github.com>
2023-10-30 16:34:07 +0300

    feat: creating lobby code implemented but not ui side just backend

--- MakeOnlineGame/Assets/_GameFolders/Scripts/Concretes/Networks/Hosts/HostManager.cs
+++ MakeOnlineGame/Assets/_GameFolders/Scripts/Concretes/Networks/Hosts/HostManager.cs
@@ -1,7 +1,10 @@
-﻿using Cysharp.Threading.Tasks;
+﻿using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using Unity.Networking.Transport.Relay;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
 using Unity.Services.Relay;
 using Unity.Services.Relay.Models;
 using UnityEngine;
@@ -15,7 +18,8 @@ namespace MakeOnlineGame.Networks.Hosts
 
         Allocation _allocation;
         string _joinCode;
-        
+        string _lobbyId;
+
         public async UniTask StartHostAsync()
         {
             try
@@ -40,13 +44,41 @@ namespace MakeOnlineGame.Networks.Hosts
             }
 
             var unityTransport = NetworkManager.Singleton.GetComponent<UnityTransport>();
-            
+
             //unityTransport.SetRelayServerData(new RelayServerData(_allocation, "udp"));
             unityTransport.SetRelayServerData(new RelayServerData(_allocation, "dtls"));
+
+            try
+            {
+                CreateLobbyOptions createLobbyOptions = new CreateLobbyOptions();
+                createLobbyOptions.IsPrivate = false;
+                createLobbyOptions.Data = new Dictionary<string, DataObject>()
+                {
+                    { "JoinCode", new DataObject(visibility: DataObject.VisibilityOptions.Member, value: _joinCode) }
+                };
+                var lobby = await Lobbies.Instance.CreateLobbyAsync("My Lobby", MAX_CONNECTION, createLobbyOptions);
+                _lobbyId = lobby.Id;
+
+                HearthBeatLobbyAsync(15);
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogError(e);
+                return;
+            }
 
             NetworkManager.Singleton.StartHost();
 
             NetworkManager.Singleton.SceneManager.LoadScene("Game", LoadSceneMode.Single);
+        }
+
+        private async UniTask HearthBeatLobbyAsync(float waitSeconds)
+        {
+            while (true)
+            {
+                await Lobbies.Instance.SendHeartbeatPingAsync(_lobbyId);
+                await UniTask.Delay(System.TimeSpan.FromSeconds(waitSeconds), false,PlayerLoopTiming.Update);
+            }
         }
     }
 }
