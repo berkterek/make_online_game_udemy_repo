486b5d672e7c2ad4a4a3a1e9335965cba14c42e9
berkterek <42406263+berkterek@users.noreply.github.com>
2023-11-03 12:54:18 +0300

    feat: players can destroy each others and respawn again

--- MakeOnlineGame/Assets/_GameFolders/Scripts/Concretes/Controllers/TankPlayerController.cs
+++ MakeOnlineGame/Assets/_GameFolders/Scripts/Concretes/Controllers/TankPlayerController.cs
@@ -1,4 +1,5 @@
 using Cinemachine;
+using MakeOnlineGame.Combats;
 using MakeOnlineGame.Networks.Hosts;
 using Unity.Collections;
 using Unity.Netcode;
@@ -8,21 +9,36 @@ namespace MakeOnlineGame.Controllers
 {
     public class TankPlayerController : NetworkBehaviour
     {
+        [SerializeField] Health _health;
         [SerializeField] CinemachineVirtualCamera _cinemachineVirtual;
 
         public NetworkVariable<FixedString32Bytes> PlayerName = new NetworkVariable<FixedString32Bytes>();
 
+        public static event System.Action<TankPlayerController> OnPlayerSpawned;
+        public static event System.Action<TankPlayerController> OnPlayerDespawned;
+
+        public Health Health => _health;
+        
         public override void OnNetworkSpawn()
         {
             if (IsServer)
             {
                 var userData = HostSingleton.Instance.HostManager.NetworkServer.GetUserDataByClientId(OwnerClientId);
                 PlayerName.Value = userData.UserName;
+                OnPlayerSpawned?.Invoke(this);
             }
             
             if (IsOwner)
             {
                 _cinemachineVirtual.Priority = 12;
+            }
+        }
+
+        public override void OnNetworkDespawn()
+        {
+            if (IsServer)
+            {
+                OnPlayerDespawned?.Invoke(this);
             }
         }
     }    
---
+++ MakeOnlineGame/Assets/_GameFolders/Scripts/Concretes/Handlers/RespawnHandler.cs
@@ -0,0 +1,68 @@
+﻿using Cysharp.Threading.Tasks;
+using MakeOnlineGame.Controllers;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace MakeOnlineGame.Handlers
+{
+    public class RespawnHandler : NetworkBehaviour
+    {
+        [SerializeField] NetworkObject _playerPrefab;
+
+        public override void OnNetworkSpawn()
+        {
+            if (!IsServer) return;
+
+            TankPlayerController[] players = FindObjectsOfType<TankPlayerController>();
+
+            foreach (var player in players)
+            {
+                HandleOnPlayerSpawned(player);
+            }
+
+            TankPlayerController.OnPlayerSpawned += HandleOnPlayerSpawned;
+            TankPlayerController.OnPlayerDespawned += HandleOnPlayerDespawned;
+        }
+
+        public override void OnNetworkDespawn()
+        {
+            if (!IsServer) return;
+            
+            TankPlayerController.OnPlayerSpawned -= HandleOnPlayerSpawned;
+            TankPlayerController.OnPlayerDespawned -= HandleOnPlayerDespawned;
+        }
+        
+        void HandleOnPlayerSpawned(TankPlayerController value)
+        {
+            value.Health.OnDead += (health) =>
+            {
+                HandleDead(value);
+            };
+        }
+
+        void HandleOnPlayerDespawned(TankPlayerController value)
+        {
+            // ReSharper disable once EventUnsubscriptionViaAnonymousDelegate
+            value.Health.OnDead -= (health) =>
+            {
+                HandleDead(value);
+            };
+        }
+
+        private void HandleDead(TankPlayerController value)
+        {
+            Destroy(value.gameObject);
+
+            RespawnPlayerAsync(value.OwnerClientId);
+        }
+
+        private async UniTask RespawnPlayerAsync(ulong clientId)
+        {
+            await UniTask.Yield();
+
+            var playerInstance = Instantiate(_playerPrefab, SpawnPointController.GetRandomSpawnPosition(), Quaternion.identity);
+            
+            playerInstance.SpawnAsPlayerObject(clientId);
+        }
+    }
+}
